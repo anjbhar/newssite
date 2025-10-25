4c542d12d3bb279d23c5ed26f42b4596dfc0b0bd
AJ Bhardwaj <axb7875@gmail.com>
2025-10-24 22:35:10 -0400

    Add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,18 @@ import tailwind from "@astrojs/tailwind";
 
 import sanity from "@sanity/astro";
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   integrations: [tailwind(), sanity({
     projectId: 'gdycm8ug',
     dataset: 'production',
     useCdn: false,
     apiVersion: '2024-03-20',
-  })]
-}); 
+  })],
+
+  adapter: netlify()
+});
